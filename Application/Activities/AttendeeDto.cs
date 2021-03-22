6a1f056fba0effea09dd9757eac7f7a744454da6
mohdnoorshawal <mohdnoorshawal@ymail.com>
2021-03-22 18:12:55 +0800

    End of Section 16

--- Application/Activities/AttendeeDto.cs
+++ Application/Activities/AttendeeDto.cs
@@ -1,8 +1,8 @@
-namespace Application.Profiles
+namespace Application.Activities
 {
-    public class Profile
+    public class AttendeeDto
     {
-        public string Username { get; set; }    
+         public string Username { get; set; }    
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
--- Persistence/Migrations/20210319100427_AddCanceledlProperty.cs
+++ Persistence/Migrations/20210319100427_AddCanceledlProperty.cs
@@ -6,12 +6,19 @@ namespace Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            //  migrationBuilder.AddColumn<bool>(
+            //     name: "IsCancelled",
+            //     table: "Activities",
+            //     type: "INTEGER",
+            //     nullable: false,
+            //     defaultValue: false);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            //  migrationBuilder.DropColumn(
+            //     name: "IsCancelled",
+            //     table: "Activities");
         }
     }
 }
